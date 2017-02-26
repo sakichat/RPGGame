4215879f9a89b29f2aef9c2f68479e0453f3d147
sakichat <saki28chat@gmail.com>
2017-02-26 11:09:08 -0500

    validator
    validator

---
+++ src/main/java/map/validator/MapValidatorEntrance.java
@@ -0,0 +1,13 @@
+package map.validator;
+
+import map.MapValidator;
+
+/**
+ * Created by Saki on 2017/2/20.
+ */
+public class MapValidatorEntrance extends MapValidator{
+    @Override
+    public boolean validate() {
+        return super.validate();
+    }
+}
---
+++ src/main/java/map/validator/MapValidatorInnerConnection.java
@@ -0,0 +1,13 @@
+package map.validator;
+
+import map.MapValidator;
+
+/**
+ * Created by Saki on 2017/2/20.
+ */
+public class MapValidatorInnerConnection extends MapValidator{
+    @Override
+    public boolean validate() {
+        return super.validate();
+    }
+}
