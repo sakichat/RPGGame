ebc7c9c498733be4401f9652715088f12ce78165
GuHan99 <zgb0705@gmail.com>
2017-02-27 10:26:51 -0500

    test

---
+++ src/test/java/persistence/FileManagerTest.java
@@ -0,0 +1,25 @@
+package persistence;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by GU_HAN on 2017-02-27.
+ */
+public class FileManagerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void nameToFileName() throws Exception {
+
+    }
+
+    @Test
+    public void fileNameToName() throws Exception {
+
+    }
+
+}
