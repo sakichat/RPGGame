4e4b1f592080bf6bf485363feba067c365455f29
qikaiqk <qikaiqk@gmail.com>
2017-02-28 12:34:25 -0500

    ChestControlView-done

--- src/main/java/ui/controlView/ChestControlView.java
+++ src/main/java/ui/controlView/ChestControlView.java
@@ -1,9 +1,68 @@
 package ui.controlView;
 
+import game.Chest;
 import ui.view.View;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Penelope on 17/2/28.
  */
 public class ChestControlView extends View {
+
+    private Chest chest;
+
+    public Chest getChest() {
+        return chest;
+    }
+
+    public void setChest(Chest chest) {
+        this.chest = chest;
+    }
+
+    public ChestControlView() {
+        this.setSize(180,560);
+        initSubviews();
+    }
+
+    public void initSubviews() {
+
+        JLabel jLabel;
+
+        jLabel = new JLabel();
+        jLabel.setSize(160, 40);
+        jLabel.setLocation(10, 10);
+        add(jLabel);
+        jLabel.setText(MainControlView.CHEST);
+        JLabel topicLabel = new JLabel();
+        topicLabel = jLabel;
+
+        jLabel = new JLabel();
+        jLabel.setSize(160, 40);
+        jLabel.setLocation(10, 60);
+        add(jLabel);
+        jLabel.setText(chest.getEquipment().getName());
+        JLabel chestNamecLabel = new JLabel();
+        chestNamecLabel = jLabel;
+
+        JButton jButton;
+
+        jButton = new JButton();
+        jButton.setSize(160, 40);
+        jButton.setLocation(10, 110);
+        add(jButton);
+        jButton.setText(MainControlView.REMOVE);
+        JButton removeButton = new JButton();
+        removeButton = jButton;
+
+        removeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+    }
 }
