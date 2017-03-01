79951b079b221fb6926129dcb37316b900ada003
sakichat <saki28chat@gmail.com>
2017-03-01 00:05:21 -0500

    map4
    map4

--- src/main/java/map/GameMap.java
+++ src/main/java/map/GameMap.java
@@ -8,123 +8,9 @@ import java.util.Queue;
  * Created by Saki on 2017/2/20.
  */
 public class GameMap {
+    private String name;
     private int size;
     private Cell[][] cells = new Cell[size][size];
-    private String name;
-    private Point exitPoint;
-    private Point enterPoint;
-
-    public GameMap() {
-    }
-
-    public GameMap(int size, Cell[][] cells, String name) {
-        this.size = size;
-        this.cells = cells;
-        this.name = name;
-    }
-
-//    public void addContent(Content content, Point location){
-//        int x = location.getX();
-//        int y = location.getY();
-//        cells[x][y].content = content;
-//    }
-//
-//    public void removeContent(Point location){
-//        int x = location.getX();
-//        int y = location.getY();
-//        cells[x][y].content = null;
-//    }
-//
-//    public Content getContent(Point location){
-//        Content content;
-//
-//        int x = location.getX();
-//        int y = location.getY();
-//        content = cells[x][y].content;
-//
-//        return content;
-//    }
-
-//    public boolean hasContent(Point location){
-//        int x = location.getX();
-//        int y = location.getY();
-//
-//        if(cells[x][y].content != null){
-//            return true;
-//        }
-//
-//        return false;
-//    }
-
-    public Cell getCell(Point location){
-        Cell cell;
-
-        int x = location.getX();
-        int y = location.getY();
-
-        cell = cells[x][y];
-
-        return cell;
-    }
-
-//    public List bfsSearch(Point source, int depth){
-//        LinkedList<Cell> searchCell = new LinkedList<>();
-//
-//        int x = source.getX();
-//        int y = source.getY();
-//
-//        searchCell.add(cells[x][y]);
-//
-//        Queue<Point> searchResult = new LinkedList<>();
-//
-//        LinkedList<Point> neighbour;
-//        neighbour = source.directions();
-//        for (int i = 0; i < 4; i++) {
-//            int neiX = neighbour.get(i).getX();
-//            int neiY = neighbour.get(i).getY();
-//            searchResult.add(neighbour.get(i));
-//            searchCell.add(cells[neiX][neiY]);
-//            neighbour.clear();
-//        }
-//
-//        if(depth == 0){
-//            while(searchResult != null){
-//                Point newSource = searchResult.poll();
-//                neighbour = newSource.directions();
-//                for (int i = 0; i < neighbour.size(); i++) {
-//                    int neiX = neighbour.get(i).getX();
-//                    int neiY = neighbour.get(i).getY();
-//                    if(cells[neiX][neiY].content == null){
-//                        for (Cell cell : searchCell) {
-//                            if(!cells[neiX][neiY].equals(cell)){
-//                                searchResult.add(neighbour.get(i));
-//                                searchCell.add(cells[neiX][neiY]);
-//                            }
-//                        }
-//                    }
-//                }
-//                neighbour.clear();
-//            }
-//        }else{
-//            for (int i = 0; i < depth - 1; i++) {
-//                if(searchResult != null){
-//                    Point newSource = searchResult.poll();
-//                    neighbour = newSource.directions();
-//                    for (int j = 0; j < 4; j++) {
-//                        int neiX = neighbour.get(j).getX();
-//                        int neiY = neighbour.get(j).getY();
-//                        if(cells[neiX][neiY].content == null){
-//                            searchResult.add(neighbour.get(j));
-//                            searchCell.add(cells[neiX][neiY]);
-//                            neighbour.clear();
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//
-//        return searchCell;
-//    }
 
     public int getSize() {
         return size;
@@ -132,6 +18,9 @@ public class GameMap {
 
     public void setSize(int size) {
         this.size = size;
+        Cell cell = new Cell();
+        Point location = new Point();
+        addCell(cell , location);
     }
 
     public String getName() {
@@ -142,11 +31,92 @@ public class GameMap {
         this.name = name;
     }
 
-    public Point getExitPoint() {
-        return exitPoint;
+    public GameMap() {
     }
 
-    public Point getEnterPoint() {
-        return enterPoint;
+    public GameMap(int size, Cell[][] cells, String name) {
+        this.size = size;
+        this.cells = cells;
+        this.name = name;
+    }
+
+    public void addCell(Cell cell, Point location){
+        int x = location.getX();
+        int y = location.getY();
+        cells[x][y] = cell;
+    }
+
+    public void removeCell(Point location){ //传参？
+        int x = location.getX();
+        int y = location.getY();
+        cells[x][y] = null;
+    }
+
+    public Cell getCell(Point location){
+
+        Cell cell;
+
+        int x = location.getX();
+        int y = location.getY();
+        cell = cells[x][y];
+
+        return cell;
+    }
+
+    public boolean hasCell(Point location){
+
+        if(getCell(location) != null){
+            return true;
+        }
+
+        return false;
+    }
+
+    public void moveCell(Point startPoint, Point endPoint){
+
+        int startX = startPoint.getX();
+        int startY = startPoint.getY();
+        int endX = endPoint.getX();
+        int endY = endPoint.getY();
+
+        Cell cell = cells[startX][startY];
+        cells[startX][startY] = null;
+        cells[endX][endY] = cell;
+    }
+
+    public LinkedList<Entrance> getEntrances(){
+        LinkedList<Entrance> entrances = new LinkedList<>();
+        Cell entranceCell;
+        Entrance entrancePoint;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (cells[i][j] instanceof Entrance){
+                    entranceCell = cells[i][j];
+                    entrancePoint = (Entrance) entranceCell;
+                    entrances.add(entrancePoint);
+                }
+            }
+        }
+
+        return entrances;
+    }
+
+    public LinkedList<Exit> getExits() {
+        LinkedList<Exit> exits = new LinkedList<>();
+        Cell exitCell;
+        Exit exitPoint;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (cells[i][j] instanceof Entrance){
+                    exitCell = cells[i][j];
+                    exitPoint = (Exit) exitCell;
+                    exits.add(exitPoint);
+                }
+            }
+        }
+
+        return exits;
     }
 }
