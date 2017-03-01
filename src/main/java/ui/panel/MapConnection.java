5e2b5cd17a0e699533c8a38c15ce06d935e91955
Siyu Chen <willing8086@gmail.com>
2017-02-28 20:40:47 -0500

    Map Connection Labels

--- src/main/java/ui/panel/MapConnection.java
+++ src/main/java/ui/panel/MapConnection.java
@@ -1,5 +1,7 @@
 package ui.panel;
 
+import javax.swing.*;
+
 /**
  * Created by Penelope on 17/2/28.
  */
@@ -12,7 +14,22 @@ public class MapConnection extends Panel {
         title = "Map Connection";
     }
 
-    protected void initSubviews() {
+    private JLabel label;
 
+    protected void initSubviews() {
+        label = new JLabel("ID", JLabel.RIGHT);
+        label.setSize(40, 40);
+        label.setLocation(100, 30);
+        add(label);
+
+        label = new JLabel("Map Name", JLabel.LEFT);
+        label.setSize(160, 40);
+        label.setLocation(150, 30);
+        add(label);
+
+        label = new JLabel("Exit Connection", JLabel.LEFT);
+        label.setSize(160, 40);
+        label.setLocation(320, 30);
+        add(label);
     }
 }
