d40b730b2139007ffc9d3cf5c3702deabf3e4097
sakichat <saki28chat@gmail.com>
2017-03-01 11:42:49 -0500

    campaign1
    campaign1

---
+++ src/main/java/map/Campaign.java
@@ -0,0 +1,32 @@
+package map;
+
+import java.util.LinkedList;
+
+/**
+ * Created by Saki on 2017/3/1.
+ */
+public class Campaign {
+    private String name;
+    private LinkedList<MapConnection> connections = new LinkedList<>();
+
+    public void addConnection(GameMap map){
+        MapConnection connection = new MapConnection();
+        connection.setMap(map);
+        connections.add(connection);
+
+        connection.setId(connections.size());
+        connection.setTargetId(connections.size() + 1);
+    }
+
+    public void removeConnection(int id){
+        
+    }
+
+    public MapConnection getConnection(int id){
+        return null;
+    }
+
+    public boolean validate(){
+        return true;
+    }
+}
