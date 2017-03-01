5bf01f8a0e49c705a9e88fe1bc3da5ccb8088bfb
sakichat <saki28chat@gmail.com>
2017-03-01 00:10:22 -0500

    map6
    map6

--- src/main/java/map/validator/MapValidatorExit.java
+++ src/main/java/map/validator/MapValidatorExit.java
@@ -1,13 +1,20 @@
 package map.validator;
 
+import map.GameMap;
 import map.MapValidator;
 
 /**
  * Created by Saki on 2017/2/20.
  */
 public class MapValidatorExit extends MapValidator{
+    GameMap gameMap = new GameMap();
+
     @Override
     public boolean validate() {
-        return super.validate();
+        if (gameMap.getExits() != null){
+            return true;
+        }
+
+        return false;
     }
 }
