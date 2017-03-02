453f3b67eb8a3d18e1c148a2461664532a272430
GuHan99 <zgb0705@gmail.com>
2017-03-02 11:27:42 -0500

    campaign test case create

---
+++ src/test/java/map/CampaignTest.java
@@ -0,0 +1,23 @@
+package map;
+
+import game.Simulation;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by GU_HAN on 2017-03-02.
+ */
+public class CampaignTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void mapValidatorConnection() throws Exception {
+        Campaign campaign = Simulation.campaign1();
+
+
+    }
+
+}
