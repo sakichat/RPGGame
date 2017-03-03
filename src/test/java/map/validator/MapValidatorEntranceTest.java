7de10b2856f48f96c8498344a5d9cca14667774e
GuHan99 <zgb0705@gmail.com>
2017-03-02 21:01:41 -0500

    test case

--- src/test/java/map/validator/MapValidatorEntranceTest.java
+++ src/test/java/map/validator/MapValidatorEntranceTest.java
@@ -1,8 +1,7 @@
 package map.validator;
 
 import game.Simulation;
-import map.GameMap;
-import map.Point;
+import map.*;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,56 +21,66 @@ public class MapValidatorEntranceTest {
     @Test
     public void testNoEntrance() throws Exception{
         Point point = new Point(3, 3);
-        GameMap gameMap2 = Simulation.gameMap1();
-        gameMap2.removeCell(point);
+        GameMap gameMap1 = Simulation.gameMap1();
+        gameMap1.removeCell(point);
 
-//        MapValidatorEntrance mapValidatorEntrance = new MapValidatorEntrance();
-//        mapValidatorEntrance.setGameMap(gameMap2);
-
-        Assert.assertEquals(false, gameMap2.validate());
+        Assert.assertEquals(GameMap.VALIDATION_ERROR_NO_ENTRANCE, gameMap1.validate());
     }
 
     /*
-     * This method tests if the map has only one entrance.
+     * This case tests if the map can have more than one entrance.
      */
-//    @Test
-//    public void validateEnrance() throws Exception{
-//        MapValidatorEntrance mapValidatorEntrance = new MapValidatorEntrance();
-//        mapValidatorEntrance.setGameMap(Simulation.gameMap1());
-//
-//        Assert.assertEquals(true, mapValidatorEntrance.validate());
-//    }
-//
-//    /*
-//     * This method tests if the map have two entrances.
-//     */
-//    @Test
-//    public void validateEntrance2() throws Exception{
-//        GameMap gameMap2 = Simulation.gameMap1();
-//        Point point = new Point(0, 0);
-//        Entrance entrance = new Entrance();
-//        gameMap2.addCell(entrance, point);
-//
-//        MapValidatorEntrance mapValidatorEntrance = new MapValidatorEntrance();
-//        mapValidatorEntrance.setGameMap(gameMap2);
-//
-//        Assert.assertEquals(false, mapValidatorEntrance.validate());
-//
-//    }
-//
-//    /*
-//     * This case test if the campaign can have no entrance.
-//     */
-//    @Test
-//    public void validateEntrance3() throws Exception{
-//        Point point = new Point(3, 3);
-//        GameMap gameMap2 = Simulation.gameMap1();
-//        gameMap2.removeCell(point);
-//
-//        MapValidatorEntrance mapValidatorEntrance = new MapValidatorEntrance();
-//        mapValidatorEntrance.setGameMap(gameMap2);
-//
-//        Assert.assertEquals(false, mapValidatorEntrance.validate());
-//    }
+    @Test
+    public void testTooMuchEntrance() throws Exception{
+        Point point = new Point(0, 0);
+        Entrance entrance = new Entrance();
+        GameMap gameMap2 = Simulation.gameMap1();
+        gameMap2.addCell(entrance, point);
+
+        Assert.assertEquals(GameMap.VALIDATION_ERROR_TOO_MUCH_ENTRANCE, gameMap2.validate());
+    }
+
+    /*
+     * This case tests if the map has no exits.
+     */
+    @Test
+    public void testNoExits() throws Exception{
+        Point point = new Point(1, 1);
+        GameMap gameMap3 = Simulation.gameMap1();
+        gameMap3.removeCell(point);
+
+        Assert.assertEquals(GameMap.VALIDATION_ERROR_NO_EXIT, gameMap3.validate());
+    }
+
+    /*
+     * This case tests if the map has too much exits.
+     */
+    @Test
+    public void testTooMuchExits() throws Exception{
+        Point point = new Point(0, 1);
+        GameMap gameMap4 = Simulation.gameMap1();
+        Exit exit = new Exit();
+        gameMap4.addCell(exit, point);
+
+        Assert.assertEquals(GameMap.VALIDATION_ERROR_TOO_MUCH_EXIT, gameMap4.validate());
+    }
+
+    /*
+     * This case tests if the map can be reached to the exit.
+     */
+    @Test
+    public void testConnection() throws Exception{
+        GameMap gameMap5 = Simulation.gameMap1();
+        Obstacle obstacle1 = new Obstacle();
+        Obstacle obstacle2 = new Obstacle();
+        Point point1 = new Point(0, 0);
+        Point point2 = new Point(2, 2);
+
+        gameMap5.addCell(obstacle1, point1);
+        gameMap5.addCell(obstacle2, point2);
+
+        Assert.assertEquals(GameMap.VALIDATION_SUCCESS, gameMap5.validate());
+    }
+
 
 }
