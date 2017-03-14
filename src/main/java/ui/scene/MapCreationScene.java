ecc316e59ac61742a4bac637410d902ff79ad0a8
Siyu Chen <willing8086@gmail.com>
2017-03-13 22:44:33 -0400

    Create map in typing

--- src/main/java/ui/scene/MapCreationScene.java
+++ src/main/java/ui/scene/MapCreationScene.java
@@ -12,7 +12,8 @@ import java.awt.event.ActionListener;
  * @version 0.1
  */
 public class MapCreationScene extends Scene {
-    private int size;
+    private int width;
+    private int height;
 
     /**
      * This init() method overrides that in superclass to set up own properties for this subclass
@@ -33,81 +34,40 @@ public class MapCreationScene extends Scene {
     protected void initSubviews() {
 
         JLabel label;
-        JButton button;
+        JTextField textField;
 
         label = new JLabel("Name", JLabel.RIGHT);
         label.setSize(120, 40);
         label.setLocation(20, 20);
         contentView.add(label);
-        JLabel mapName = label;
 
-        JTextField nameField = new JTextField();
-        nameField.setSize(160, 40);
-        nameField.setLocation(150, 20);
-        contentView.add(nameField);
+        textField = new JTextField();
+        textField.setSize(160, 40);
+        textField.setLocation(150, 20);
+        contentView.add(textField);
+        JTextField nameField = textField;
 
-        label = new JLabel("Size", JLabel.RIGHT);
+        label = new JLabel("Width", JLabel.RIGHT);
         label.setSize(120, 40);
         label.setLocation(20, 70);
         contentView.add(label);
 
-        label = new JLabel();
-        label.setSize(200, 40);
-        label.setLocation(150, 70);
-        label.setText("4 x 4");
+        textField = new JTextField();
+        textField.setSize(120, 40);
+        textField.setLocation(150, 70);
+        contentView.add(textField);
+        JTextField widthField = textField;
+
+        label = new JLabel("Height", JLabel.RIGHT);
+        label.setSize(120, 40);
+        label.setLocation(20, 120);
         contentView.add(label);
-        JLabel sizeLabel = label;
-        size = 4;
 
-        button = new JButton();
-        button.setSize(100, 40);
-        button.setLocation(150, 120);
-        contentView.add(button);
-        button.setText("4 x 4");
-        JButton smallSize = button;
-
-        smallSize.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                sizeLabel.setText(smallSize.getText());
-                size = 4;
-
-                repaint();
-            }
-        });
-
-        button = new JButton("8 x 8");
-        button.setSize(100, 40);
-        button.setLocation(260, 120);
-        contentView.add(button);
-        JButton mediumSize = button;
-
-        mediumSize.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                sizeLabel.setText(mediumSize.getText());
-                size = 8;
-
-                repaint();
-            }
-        });
-
-
-        button = new JButton("12 x 12");
-        button.setSize(100, 40);
-        button.setLocation(370, 120);
-        contentView.add(button);
-        JButton largeSize = button;
-
-        largeSize.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                sizeLabel.setText(largeSize.getText());
-                size = 12;
-
-                repaint();
-            }
-        });
+        textField = new JTextField();
+        textField.setSize(120, 40);
+        textField.setLocation(150, 120);
+        contentView.add(textField);
+        JTextField heightField = textField;
 
         JButton createButton = new JButton("Create");
         createButton.setSize(160, 40);
@@ -128,7 +88,8 @@ public class MapCreationScene extends Scene {
             public void actionPerformed(ActionEvent e) {
                 GameMap gameMap = new GameMap();
                 gameMap.setName(nameField.getText());
-                gameMap.setSize(size);
+//                gameMap.setWidth(widthField.getText());
+//                gameMap.setHeight(heightField.getText());
                 MapEditingScene mapEditingScene = new MapEditingScene();
                 mapEditingScene.setGameMap(gameMap);
                 MapCreationScene.this.navigationView.push(mapEditingScene);
