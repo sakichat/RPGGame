4196d7b11620d26d6badf9ff53bfafe98f2945ba
GuHan99 <zgb0705@gmail.com>
2017-03-20 19:26:02 -0400

    test creat

---
+++ src/test/java/logic/PlayTest.java
@@ -0,0 +1,18 @@
+package logic;
+
+import org.junit.Test;
+
+/**
+ * Created by GU_HAN on 2017-03-20.
+ */
+public class PlayTest {
+    @Test
+    public void moveTest() throws Exception {
+
+    }
+
+    @Test
+    public void cannotMoveTest() throws Exception {
+
+    }
+}
