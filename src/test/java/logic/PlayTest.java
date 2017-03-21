686a931086983e66ec6daf8ed3afc3e596222bf3
GuHan99 <zgb0705@gmail.com>
2017-03-21 01:31:40 -0400

    confused  play test

--- src/test/java/logic/PlayTest.java
+++ src/test/java/logic/PlayTest.java
@@ -1,6 +1,6 @@
 package logic;
 
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
 import persistence.CampaignFileManager;
 import persistence.PlayerFileManager;
@@ -9,20 +9,39 @@ import persistence.PlayerFileManager;
  * Created by GU_HAN on 2017-03-20.
  */
 public class PlayTest {
-    @Before
-    public void setUp() throws Exception {
+
+//    private Play play;
+//    @Before
+//    public void setUp() throws Exception {
+//        Campaign campaign = CampaignFileManager.read("caribean_sea");
+//        Player player = PlayerFileManager.read("asheley");
+//        Play play = new Play();
+//        play.setCampaign(campaign);
+//        play.setPlayer(player);
+//        play.setDirection(new Point(1, 0));
+//
+//        this.play = play;
+//
+//    }
+
+    @Test
+    public void moveTest() throws Exception {
         Campaign campaign = CampaignFileManager.read("caribean_sea");
         Player player = PlayerFileManager.read("asheley");
         Play play = new Play();
         play.setCampaign(campaign);
         play.setPlayer(player);
-        play.setDirection(new Point(0, 1));
+        play.setDirection(Point.DIRECTION_LEFT);
+        play.resolveMap();
+        System.out.println(play.getPlayer().getLocation());
 
-    }
+        play.move();
+//        play.move();
+//        play.move();
 
-    @Test
-    public void moveTest() throws Exception {
+        System.out.println(play.getPlayer().getLocation());
 
+        Assert.assertEquals(true, true);
     }
 
     @Test
