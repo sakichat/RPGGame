f2f2914de4ff83ee32b441d7670d453b68b04371
Li Zhen <thereaghostflash@gmail.com>
2017-03-21 22:13:15 -0400

    lizhen
    lizhen

--- src/main/java/logic/BullyBuilder.java
+++ src/main/java/logic/BullyBuilder.java
@@ -2,7 +2,7 @@ package logic;
 
 /**
  * Created by lizhen on 2017-03-21.
- * this is the class that build  type Bully
+ * this is the class that build  type  of Bully
  *@version 0.2
  */
 public class BullyBuilder extends PlayerBuilder {
