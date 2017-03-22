10a5ec2742816353270e95bbdb191213fcb91e15
Li Zhen <thereaghostflash@gmail.com>
2017-03-21 21:49:35 -0400

    lizhen
    lizhen

--- src/main/java/logic/PlayerBuilder.java
+++ src/main/java/logic/PlayerBuilder.java
@@ -40,7 +40,7 @@ public abstract class PlayerBuilder {
     abstract void setPlayerType();
 
     /**
-     * this is a abstract method 
+     * this is a abstract method to 
      */
     abstract void generateAbilities();
 
