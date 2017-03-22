3940587493f6ad3059ea3376e2bd1a80d6fce9f4
Li Zhen <thereaghostflash@gmail.com>
2017-03-21 22:15:42 -0400

    lizhen
    lizhen

--- src/main/java/logic/NimbleBuilder.java
+++ src/main/java/logic/NimbleBuilder.java
@@ -2,7 +2,7 @@ package logic;
 
 /**
  * Created by Li Zhen on 2017-03-21.
- * this is the class to build typle of Nimble
+ * this is the class to build typle of Nimble Player
  */
 public class NimbleBuilder extends PlayerBuilder {
     @Override
