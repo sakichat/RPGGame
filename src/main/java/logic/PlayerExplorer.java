4db60bc3a0c3973ba2ccea2d85bf28376007db8d
Li Zhen <thereaghostflash@gmail.com>
2017-03-21 22:41:19 -0400

    lizhen
    lizhen

--- src/main/java/logic/PlayerExplorer.java
+++ src/main/java/logic/PlayerExplorer.java
@@ -9,7 +9,7 @@ public class PlayerExplorer {
     private PlayerBuilder playerBuilder;
 
     /**
-     * this is the method 
+     * this is the method that 
      * @param playerBuilder
      */
     public void setPlayerBuilder(PlayerBuilder playerBuilder) {
