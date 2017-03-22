ef0f32c08951bf5f0cfcdd46db34df2e8d9dee8d
Li Zhen <thereaghostflash@gmail.com>
2017-03-21 22:49:07 -0400

    @version 0.2
    @version 0.2

--- src/main/java/logic/PlayerExplorer.java
+++ src/main/java/logic/PlayerExplorer.java
@@ -3,7 +3,7 @@ package logic;
 /**
  * Created by Li Zhen on 2017-03-21.
  * this is the class that command the different builders of player
- * @version 
+ * @version 0.2
  */
 public class PlayerExplorer {
 
