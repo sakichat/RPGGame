cc20512a2b0cbecfc905f0d5c0fbb2c8155221c6
Li Zhen <thereaghostflash@gmail.com>
2017-03-21 22:29:08 -0400

    lizhen
    lizhen

--- src/main/java/logic/TankBuilder.java
+++ src/main/java/logic/TankBuilder.java
@@ -2,7 +2,7 @@ package logic;
 
 /**
  * Created by Li Zhen on 2017-03-21.
- * this is the class that build type
+ * this is the class that build type of 
  */
 public class TankBuilder extends PlayerBuilder {
 
