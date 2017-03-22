6591bfe0c6b22b947bf5860fc73b44abe0a69f9c
Li Zhen <thereaghostflash@gmail.com>
2017-03-21 22:02:54 -0400

    lizhen
    lizhen

--- src/main/java/logic/BullyBuilder.java
+++ src/main/java/logic/BullyBuilder.java
@@ -6,7 +6,7 @@ package logic;
  */
 public class BullyBuilder extends PlayerBuilder {
     /**
-     * this is the method to
+     * this is the method to set
      */
     @Override
     void setPlayerType() {
