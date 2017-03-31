c710dfa781fad768178f8f430eef3aba1bd15cde
GuHan99 <zgb0705@gmail.com>
2017-03-31 17:05:43 -0400

    builder test

---
+++ src/test/java/logic/PlayerExplorerTest.java
@@ -0,0 +1,33 @@
+package logic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by GU_HAN on 2017-03-31.
+ * @author GU_HAN
+ * @version 0.2
+ *
+ * This class tests the Builder pattern correctly assigns ability scores.
+ */
+public class PlayerExplorerTest {
+    @Before
+    public void setUp() throws Exception {
+        
+    }
+
+    @Test
+    public void setPlayerBuilder() throws Exception {
+
+    }
+
+    @Test
+    public void constructPlayer() throws Exception {
+
+    }
+
+    @Test
+    public void getPlayer() throws Exception {
+
+    }
+}
