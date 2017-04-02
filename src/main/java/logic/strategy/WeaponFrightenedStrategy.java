744cca703f67ad312c9cc5d9ffd93d3303b11def
qikaiqk <qikaiqk@gmail.com>
2017-04-02 15:28:34 -0400

    two more strategy creation (empty)

---
+++ src/main/java/logic/strategy/WeaponFrightenedStrategy.java
@@ -0,0 +1,12 @@
+package logic.strategy;
+
+/**
+ * Created by Kai QI on 2017/4/2.
+ */
+public class WeaponFrightenedStrategy implements PlayerStrategy {
+
+    @Override
+    public void turn() {
+
+    }
+}
