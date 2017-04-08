0e6e35a6b0a8e8927c8183fd47d61bfcbf4e3808
Siyu Chen <willing8086@gmail.com>
2017-04-08 18:43:24 -0400

    playerCellView update

--- src/main/java/ui/view/PlayerCellView.java
+++ src/main/java/ui/view/PlayerCellView.java
@@ -1,42 +1,47 @@
 package ui.view;
 
+import logic.player.Player;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.Observable;
+import java.util.Observer;
 
 /**
  * Created by Penelope on 2017-04-01.
  */
-public class PlayerCellView extends View{
-    private String name;
-    private String effectName;
+public class PlayerCellView extends View implements Observer{
+    private Player player;
+    private double hitPointUnit;
 
-    @Override
-    public String getName() {
-        return name;
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+        player.addObserver(this);
     }
 
     @Override
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEffectName() {
-        return effectName;
-    }
-
-    public void setEffectName(String effectName) {
-        this.effectName = effectName;
+    public void update(Observable o, Object x) {
+        if (x.equals(Player.HP_CHANGE)) {
+            repaint();
+        }
     }
 
     @Override
     public void paint(Graphics g) {
+        g.setColor(new Color(0xDB2C5A));
+        g.fillRect(0, 0, getPlayer().getHp(), 0);
+
         g.setColor(new Color(0xF4F4F4));
         g.drawRect(0, 0, 40, 5);
 
-        ImageIcon effectImageView = new ImageIcon("data/images/" + getEffectName());
+        ImageIcon effectImageView = new ImageIcon("data/images/" + getPlayer().getEffects());
         g.drawImage(effectImageView.getImage(), 0, 0, null);
 
-        ImageIcon playerImageView = new ImageIcon("data/images/" + getName());
+        ImageIcon playerImageView = new ImageIcon("data/images/" + getPlayer().getImageName());
         g.drawImage(playerImageView.getImage(), 0, 0, null);
 
     }
