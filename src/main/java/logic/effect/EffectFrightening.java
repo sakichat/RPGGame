686dfa20eb18a26d3274ee99ca3180917f1e9167
sakichat <saki28chat@gmail.com>
2017-04-08 20:48:07 -0400

    frightened effect
    frightened effect

--- src/main/java/logic/effect/EffectFrightening.java
+++ src/main/java/logic/effect/EffectFrightening.java
@@ -1,14 +1,20 @@
 package logic.effect;
 
 
+import logic.turn.TurnStrategy;
+import logic.turn.TurnStrategyFrightened;
+
 public class EffectFrightening extends Effect {
+    private TurnStrategy turnStrategy;
+
     @Override
     protected void didAttach() {
-        // TODO: 08/04/2017
+        turnStrategy = onPlayer.getStrategy();
+        onPlayer.setStrategy(new TurnStrategyFrightened());
     }
 
     @Override
     protected void willDetach() {
-        // TODO: 08/04/2017  
+        onPlayer.setStrategy(turnStrategy);
     }
 }
