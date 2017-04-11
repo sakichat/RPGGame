6b2872df4dd5fb31c979adef040d56231603355a
sakichat <saki28chat@gmail.com>
2017-04-10 23:58:40 -0400

    frightened strategy
    frightened strategy

--- src/main/java/logic/turn/TurnStrategyFrightened.java
+++ src/main/java/logic/turn/TurnStrategyFrightened.java
@@ -1,12 +1,24 @@
 package logic.turn;
 
+import logic.Play;
+import logic.map.GameMap;
+import logic.map.GameMapGraph;
 import logic.map.Point;
+
+import java.util.List;
 
 public class TurnStrategyFrightened extends TurnStrategy {
 
     @Override
     protected Point preferredNextLocation() {
-        // TODO: 10/04/2017
+        GameMapGraph gameMapGraph = Play.getCurrentPlay().getCurrentMap().getGraph();
+        List<Point> points = gameMapGraph.pointsInRange(player.getLocation(), player.getRangeForMove());
+
+        if (points.size() != 0) {
+            Point result = points.get((int)(Math.random() * points.size()));
+            return result;
+        }
+
         return null;
     }
 
