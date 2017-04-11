ec4e4bc08cc04a2f83e3007685399a946153da5d
qikaiqk <qikaiqk@gmail.com>
2017-04-10 20:40:11 -0400

    .

--- src/main/java/logic/turn/TurnStrategy.java
+++ src/main/java/logic/turn/TurnStrategy.java
@@ -1,5 +1,39 @@
 package logic.turn;
 
-public class TurnStrategy {
+import logic.Play;
+import logic.map.Cell;
+import logic.player.Player;
 
+public class TurnStrategy {
+    private Player player;
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public void turn(){
+
+    }
+    
+    private boolean attackTargetsInNear(){
+        // TODO: 08/04/2017
+        return false;
+    }
+
+    protected boolean couldAttack(Player targetPlayer) {
+        return true;
+    }
+
+    private boolean interactTargetsInNear(){
+        // TODO: 08/04/2017
+        return false;
+    }
+
+    protected boolean couldInteract(Cell cell) {
+        return true;
+    }
 }
