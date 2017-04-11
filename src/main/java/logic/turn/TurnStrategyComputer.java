d80f454ba296264235947d10422ae32ca00923a2
Li Zhen <thereaghostflash@gmail.com>
2017-04-11 00:20:26 -0400

    lizhen
    listen

--- src/main/java/logic/turn/TurnStrategyComputer.java
+++ src/main/java/logic/turn/TurnStrategyComputer.java
@@ -1,31 +1,95 @@
 package logic.turn;
 
+import logic.Play;
+import logic.map.Cell;
+import logic.map.GameMap;
+import logic.map.GameMapGraph;
 import logic.map.Point;
+import logic.player.Player;
+
+import java.util.List;
 
 
 public class TurnStrategyComputer extends TurnStrategy {
     @Override
     public Point preferredNextLocation() {
-        return null;
+        GameMapGraph gameMapGraph = Play.getCurrentPlay().getCurrentMap().getGraph();
+        List<Point> points = gameMapGraph.pointsInRange(player.getLocation(), player.getRangeForMove());
+
+        if (points.size() != 0){
+            Point result = points.get((int)(Math.random() * points.size()));
+            return result;
+        }else {
+            return null;
+        }
     }
 
     @Override
     public boolean couldAttack(Point target) {
-        return false;
+        boolean result = false;
+        Play play = Play.getCurrentPlay();
+        GameMap gameMap = play.getCurrentMap();
+        Cell cell = gameMap.getCell(target);
+        if (cell == null){
+            return result;
+        }else {
+            if (cell.getCellType().equals(Cell.Type.PLAYER)){
+                Player targetPlayer = (Player)cell;
+                    if (targetPlayer.isAlive()){
+                        result = true;
+
+                }
+            }
+        }
+
+        return result;
     }
 
     @Override
     protected boolean couldInteract(Point target) {
-        return false;
+        boolean result = false;
+        Play play = Play.getCurrentPlay();
+        GameMap gameMap = play.getCurrentMap();
+        Cell cell = gameMap.getCell(target);
+        if (cell.getCellType().equals(Cell.Type.CHEST)){
+            result = true;
+        }
+        if (cell == null){
+            return result;
+        }else {
+            if (cell.getCellType().equals(Cell.Type.PLAYER)){
+                Player targetPlayer = (Player)cell;
+                if ((targetPlayer.getPlayerParty().equals(Player.PLAYER_PARTY_HOSTILE)){
+                    if (targetPlayer.isAlive()){
+                        result = true;
+                    }
+                }
+            }
+        }
+
+        return result;
     }
 
     @Override
     public Point preferredAttackingLocation() {
-        return null;
+        List<Point> points = attackTargetsInNear();
+        if (points.size() != 0){
+            Point result = points.get((int)(Math.random() * points.size()));
+            return result;
+        }else {
+            return null;
+        }
     }
 
     @Override
     public Point preferredInteractionLocation() {
-        return null;
+        GameMapGraph gameMapGraph = Play.getCurrentPlay().getCurrentMap().getGraph();
+        List<Point> points = interactTargetsInNear();
+        if (points.size() != 0){
+            Point result = points.get((int)(Math.random() * points.size()));
+            return result;
+        }else {
+            return null;
+        }
     }
 }
