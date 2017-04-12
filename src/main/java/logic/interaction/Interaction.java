40e3f00a955d83c5fc3049d6821a878347c78cb3
thereaghostflash <thereaghostflash@gmail.com>
2017-04-12 14:43:29 -0400

    e

--- src/main/java/logic/interaction/Interaction.java
+++ src/main/java/logic/interaction/Interaction.java
@@ -5,9 +5,7 @@ import logic.PlayRuntime;
 import logic.map.Cell;
 import logic.player.Player;
 
-/**
- * Created by Zhaozhe on 12/04/2017.
- */
+
 public abstract class Interaction<T extends Cell> {
     protected Player player;
 
--- src/main/java/logic/interaction/InteractionFactory.java
+++ src/main/java/logic/interaction/InteractionFactory.java
@@ -3,9 +3,6 @@ package logic.interaction;
 import logic.map.Cell;
 import logic.player.Player;
 
-/**
- * Created by Zhaozhe on 12/04/2017.
- */
 public class InteractionFactory {
     public Interaction interaction(Player source, Cell target){
         return null;
