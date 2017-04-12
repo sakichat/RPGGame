b1889fc489e031b2807b14a29a952ec75b81b2c1
thereaghostflash <thereaghostflash@gmail.com>
2017-04-12 14:41:39 -0400

    i

---
+++ src/main/java/logic/interaction/InteractionFactory.java
@@ -0,0 +1,13 @@
+package logic.interaction;
+
+import logic.map.Cell;
+import logic.player.Player;
+
+/**
+ * Created by Zhaozhe on 12/04/2017.
+ */
+public class InteractionFactory {
+    public Interaction interaction(Player source, Cell target){
+        return null;
+    }
+}
---
+++ src/main/java/logic/interaction/InteractionFriendlyNPC.java
@@ -0,0 +1,12 @@
+package logic.interaction;
+
+import logic.map.Cell;
+import logic.player.Player;
+
+
+public class InteractionFriendlyNPC extends Interaction<Player> {
+    @Override
+    public void interact(Player target) {
+
+    }
+}
