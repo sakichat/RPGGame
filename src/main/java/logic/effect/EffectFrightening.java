c68e2c79700c1595e014dde77c719d1c939805a7
sakichat <saki28chat@gmail.com>
2017-04-12 00:56:11 -0400

    effectFrightening play runtime
    effectFrightening play runtime

--- src/main/java/logic/effect/EffectFrightening.java
+++ src/main/java/logic/effect/EffectFrightening.java
@@ -1,14 +1,14 @@
 package logic.effect;
 
 
-import logic.Play;
+import logic.PlayRuntime;
 import logic.player.Player;
 import logic.turn.TurnStrategy;
 import logic.turn.TurnStrategyFrightened;
 
 public class EffectFrightening extends Effect {
     private TurnStrategy turnStrategy;
-    private Player combatant = Play.getCurrentPlay().getPlayer();
+    private Player combatant = PlayRuntime.currentRuntime().getPlayer();
 
     @Override
     protected void didAttach() {
