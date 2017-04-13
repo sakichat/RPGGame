de55b0a1909741cc18e9ba428c77656cc5726c68
GuHan99 <zgb0705@gmail.com>
2017-04-13 14:26:42 -0400

    strategy test

--- src/test/java/logic/turn/TurnStrategyTest.java
+++ src/test/java/logic/turn/TurnStrategyTest.java
@@ -3,6 +3,7 @@ package logic.turn;
 import logic.Play;
 import logic.PlayRuntime;
 import logic.map.Point;
+import logic.player.Player;
 import org.junit.Before;
 import org.junit.Test;
 import persistence.CampaignFileManager;
@@ -13,6 +14,7 @@ import ui.scene.PlayScene;
  * Created by GU_HAN on 2017-04-13.
  */
 public class TurnStrategyTest {
+
     @Before
     public void setUp() throws Exception {
 
@@ -26,40 +28,28 @@ public class TurnStrategyTest {
     public void aiPlayer() throws Exception {
 
         Play play = new Play();
-        play.setCampaign(CampaignFileManager.read("testcampaign"));
-        play.setMainPlayer(PlayerFileManager.read("asheley"));
+        play.setCampaign(CampaignFileManager.read("humantest"));
+        play.setMainPlayer(PlayerFileManager.read("testplayer"));
         play.getMainPlayer().setStrategy(new TurnStrategyComputer());
 
         PlayRuntime.currentRuntime().initiate(new PlayScene(), play);
 
         PlayRuntime.currentRuntime().begin();
-        Point location1 = PlayRuntime.currentRuntime().getMainPlayer().getLocation();
+
+        Player player1 = PlayRuntime.currentRuntime().getPlay().currentPlayer();
+        Point location1 = player1.getLocation();
         System.out.println(location1.getX() + " " + location1.getY());
+        player1.turn();
 
-        PlayRuntime.currentRuntime().begin();
-        PlayRuntime.currentRuntime().getPlay().currentPlayer().getLocation();
+        Player player2 = PlayRuntime.currentRuntime().getPlay().currentPlayer();
+        Point location2 = player2.getLocation();
+        System.out.println(location2.getX() + " " + location2.getY());
+        player2.turn();
 
-
-
-
-
+        Player player3 = PlayRuntime.currentRuntime().getPlay().currentPlayer();
+        Point location3 = player3.getLocation();;
+        System.out.println(location3.getX() + " " + location3.getY());
+        player3.turn();
     }
 
-    /**
-     * friendly ai
-     */
-    @Test
-    public void friendlyPlayer() throws Exception {
-
-    }
-
-    @Test
-    public void hostilePlayer() throws Exception {
-
-    }
-
-    @Test
-    public void humanPlayer() throws Exception {
-
-    }
 }
