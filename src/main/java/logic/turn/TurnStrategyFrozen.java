83115f2b360ce4fa61b259bfa2b4008d22af2929
qikaiqk <qikaiqk@gmail.com>
2017-04-12 21:50:16 -0400

    fritened&frozen done

--- src/main/java/logic/turn/TurnStrategyFrozen.java
+++ src/main/java/logic/turn/TurnStrategyFrozen.java
@@ -12,25 +12,21 @@ public class TurnStrategyFrozen extends TurnStrategy {
 
     @Override
     public boolean couldAttack(Point target) {
-        // TODO: 10/04/2017
         return false;
     }
 
     @Override
     protected boolean couldInteract(Point target) {
-        // TODO: 10/04/2017
         return false;
     }
 
     @Override
     public Point preferredAttackingLocation() {
-        // TODO: 10/04/2017
         return null;
     }
 
     @Override
     public Point preferredInteractionLocation() {
-        // TODO: 10/04/2017
         return null;
     }
 }
