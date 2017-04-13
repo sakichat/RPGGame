ced0d9eca6af818a76b73c5efa85e7845570267f
sakichat <saki28chat@gmail.com>
2017-04-12 23:12:55 -0400

    Equipment interface
    Equipment interface

--- src/main/java/logic/equipment/Equipment.java
+++ src/main/java/logic/equipment/Equipment.java
@@ -8,6 +8,9 @@ package logic.equipment;
 
 public interface Equipment {
 
+    /**
+     * static constant
+     */
     public final static String HELMET = "Helmet";
     public final static String ARMOR = "Armor";
     public final static String SHIELD = "Shield";
@@ -16,22 +19,50 @@ public interface Equipment {
     public final static String BOOTS = "Boots";
     public final static String WEAPON = "Weapon";
 
+    /**
+     * This is the method for validate
+     * @return Boolean
+     */
     boolean validate();
 
+    /**
+     * attribute and setter
+     * @return String
+     */
     String getName();
     void setName(String name);
 
+    /**
+     * attribute and setter
+     * @return String
+     */
     String getType();
     void setType(String type);
 
+    /**
+     * attribute and setter
+     * @return String
+     */
     String getEnhancedAttribute();
     void setEnhancedAttribute(String enhancedAttribute);
 
+    /**
+     * attribute and setter
+     * @return Integer
+     */
     int getEnhancedValue();
     void setEnhancedValue(int enhancedValue);
 
+    /**
+     * This is the method for adapt level
+     * @param level
+     */
     void adapt(int level);
 
+    /**
+     * attribute
+     * @return String
+     */
     String displayName();
 
 }
