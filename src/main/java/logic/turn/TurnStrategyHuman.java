687445680906a7b8aa0545d5aa000aaaf23bf282
sakichat <saki28chat@gmail.com>
2017-04-12 23:38:13 -0400

    human strategy
    human strategy

--- src/main/java/logic/turn/TurnStrategyHuman.java
+++ src/main/java/logic/turn/TurnStrategyHuman.java
@@ -1,33 +1,57 @@
 package logic.turn;
 
-import logic.map.Path;
-import logic.map.Point;
+import logic.PlayRuntime;
+import logic.map.*;
+import logic.player.Player;
+
+import java.util.List;
 
 public class TurnStrategyHuman extends TurnStrategy {
     @Override
     public Path preferredMovingPath() {
         TurnThread.waitForUser(TurnThread.UserResponse.MOVE);
+        Point targetLocation = PlayRuntime.currentRuntime().getPlay().getTargetLocation();
 
-        return new Path();
+        GameMapGraph gameMapGraph = PlayRuntime.currentRuntime().getMap().getGraph();
+
+        return gameMapGraph.path(player.getLocation(), targetLocation, player.getRangeForMove());
     }
 
     @Override
     public boolean couldAttack(Point target) {
+        PlayRuntime runtime = PlayRuntime.currentRuntime();
+        GameMap map = runtime.getMap();
+
+        Player targetPlayer = map.getPlayer(target);
+        if (targetPlayer.getPlayerParty().equals(Player.PLAYER_PARTY_HOSTILE)) {
+            return true;
+        }
+
         return false;
     }
 
     @Override
     protected boolean couldInteract(Point target) {
+        PlayRuntime runtime = PlayRuntime.currentRuntime();
+        GameMap map = runtime.getMap();
+
+        Cell targetCell = map.getCell(target);
+        if (targetCell instanceof Chest || player.isDead()){
+            return true;
+        }
+
         return false;
     }
 
     @Override
     public Point preferredAttackingLocation() {
-        return null;
+        TurnThread.waitForUser(TurnThread.UserResponse.ATTACK);
+        return PlayRuntime.currentRuntime().getPlay().getTargetLocation();
     }
 
     @Override
     public Point preferredInteractionLocation() {
-        return null;
+        TurnThread.waitForUser(TurnThread.UserResponse.INTERACT);
+        return PlayRuntime.currentRuntime().getPlay().getTargetLocation();
     }
 }
